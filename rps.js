bd7dac783132e2fa6d70f99a58935fda0d6c7f66
GrapeJuice11111 <118233081+GrapeJuice11111@users.noreply.github.com>
2023-07-20 02:01:04 +0545

    Fix The Undefined Being Consoled Error

--- rps.js
+++ rps.js
@@ -1,18 +1,16 @@
 const btns = document.querySelectorAll("button");
-const counterForComputer = 0;
-const counterForPlayer = 0;
+let counterForComputer = 0;
+let counterForPlayer = 0;
 btns.forEach(button => button.addEventListener('click', function(e){
-    if( counterForComputer > 5 || counterForPlayer > 5){
-     console.log("END");
-    }
-    else{
      var userChoice = ""
    userChoice = this.classList.value;
-   console.log(userChoice);
+   console.log(`You chose ${userChoice}`);
     function getComputerChoice(){
     let choices = ["rock","paper","scissors"];
     computerChoice = choices[Math.floor(Math.random() * 3)]; 
-    return computerChoice;
+    var choice = "";
+    choice = computerChoice;
+    return choice;
    }
    function playRound(playerSelection,computerSelection){
     if( playerSelection === computerSelection ){
@@ -43,13 +41,14 @@ btns.forEach(button => button.addEventListener('click', function(e){
         console.log("Scissors beats Paper. You win");
         counterForPlayer = counterForPlayer + 1;
     }
-}
-  console.log(playRound(userChoice,getComputerChoice()));
-}}
-));
-if (counterForComputer === 5) {
-    console.log("The computer has won. Dont worry Player is smarter than computer.");
+} 
+ if(counterForComputer < 5 || counterForComputer < 5){
+    playRound(userChoice,getComputerChoice());
+  }
+  else if(counterForComputer === 5) {
+    console.log("The computer has won. Humanity is done for.");
     }
     else if(counterForPlayer === 5){
-    console.log("The Player has won. Congratulations")
+    console.log("The Player has won. Congratulations you saved us. You are the hero");
     }
+}));
